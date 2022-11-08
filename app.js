29427ae567498bc5cfa211b935075e815d714cdf
brennanca <113802053+brennanca@users.noreply.github.com>
2022-11-08 14:57:12 -0500

    made json,

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+var msg = 'Hello World';
+console.log(msg);
 
+
+// const fs = require('fs');
+// const csv = require('csv-parser');
+
+
+// let data = [];
+// fs.createReadStream('raw_artists.csv')
+// .pipe(csv())
+// .on('data', (rows) => {
+//     data.push(rows);
+// }).on('end', () => {
+//     console.table(data);
+
+//     const result = data.filter((row) => {
+//         return row.gender == 'Female'
+//     })
+
+//     console.log(result);
+// })
