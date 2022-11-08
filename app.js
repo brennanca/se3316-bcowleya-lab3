7b1eacbbf255f95578b20d8e39d352a55a967dd2
brennanca <113802053+brennanca@users.noreply.github.com>
2022-11-08 15:07:26 -0500

    serv.js completed & csv read

--- app.js
+++ app.js
@@ -2,21 +2,24 @@ var msg = 'Hello World';
 console.log(msg);
 
 
-// const fs = require('fs');
-// const csv = require('csv-parser');
+const fs = require('fs');
+const csv = require('csv-parser');
 
 
-// let data = [];
-// fs.createReadStream('raw_artists.csv')
-// .pipe(csv())
-// .on('data', (rows) => {
-//     data.push(rows);
-// }).on('end', () => {
+let data = [];
+fs.createReadStream('lab3-data 2/raw_artists.csv')
+.pipe(csv())
+.on('data', (rows) => {
+    console.log(rows);
+    //data.push(rows);
+})
+
+// .on('end', () => {
 //     console.table(data);
 
-//     const result = data.filter((row) => {
-//         return row.gender == 'Female'
-//     })
+//     // const result = data.filter((row) => {
+//     //     return row.gender == 'Female'
+//     // })
 
-//     console.log(result);
+//     // console.log(result);
 // })
